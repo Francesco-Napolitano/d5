cb0af98d2941990cac9ad60bba33b78ad095eb57
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2024-11-29 16:25:07 +0100

    U3-W1-D5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import MyNav from './components/MyNav'
+import MyMain from './components/MyMain'
+import MyFooter from './components/MyFooter'
+import MySections from './components/MySections'
+import SectionMarvel from './components/SectionMarvel'
+import LastSection from './components/LastSection'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-dark">
+      <header>
+        <MyNav />
       </header>
+      <main className="bg-dark">
+        <MyMain />
+        <MySections />
+        <SectionMarvel />
+        <LastSection />
+      </main>
+      <footer>
+        <MyFooter />
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
